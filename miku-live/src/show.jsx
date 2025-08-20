11087477b48b6286cad903c645cb971bac39efae
NaCl <naclwww@outlook.com>
2025-08-20 18:54:57 +0800

    add Show.jsx

--- miku-live/src/show.jsx
+++ miku-live/src/show.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import Test from './show'
-import Show from './show'
 
 
-function App() {
+function Show() {
   const endRef = useRef(null)
   const showRef = useRef(null)
   const buttonRef = useRef(null)
@@ -27,7 +25,7 @@ function App() {
     const observerShow = new window.IntersectionObserver(
       ([entry]) => {
         setShow(entry.isIntersecting)
-      },{ threshold: 0.05 }
+      },{ threshold: 0.1 }
     )
     if (showRef.current) observerShow.observe(showRef.current)
 
@@ -57,20 +55,23 @@ function App() {
   const endZoneStyle = "absolute right-8 top-8 scale-100 opacity-100"
 
   return (
-    <div className='bg-gray-100 min-h-screen'>
-      {/* 首页 */}
-      <div className="h-screen flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center text-center h-full max-w-md">
-          <h1 className="text-5xl font-bold text-gray-800">Hello there</h1>
-          <p className="py-6 text-gray-800">向下滚动查看更多内容</p>
+    <div ref={showRef}>
+        <div className="h-512 text-center bg-blue-100 flex items-center justify-center text-2xl">
+            RefContent
         </div>
-      </div>
-
-      {/* 其他项 */}
-      <Show />
-      <Show />
+        <div
+            ref={endRef}
+            className="h-32 text-center bg-red-100 flex items-center justify-center text-2xl relative"
+            style={{ position: 'relative' }}
+        >
+            {show && (
+            <div ref={buttonRef} className={buttonBase +" " +(atEndZone ? endZoneStyle : floatingStyle)} style={{ pointerEvents: 'auto' }}>
+                test
+            </div>)}
+            <div className='text-center'>RefEnd</div>
+        </div>
     </div>
   )
 }
 
-export default App
+export default Show
